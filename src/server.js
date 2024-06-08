b0c8e9507283593aa16259867cfe5b8d76627e51
Vinicius Gonzalez <vinicius.gonzalez@maiscontroleerp.com.br>
2024-06-08 01:16:06 -0300

    added content disposition to cors

--- src/server.js
+++ src/server.js
@@ -25,13 +25,14 @@ app.get('/download', async (req, res) => {
         const title = info.videoDetails.title.replace(/[^a-zA-Z0-9]/g, '_');
         const stream = ytdl(url, { quality: 'highestaudio' });
         
+        res.setHeader('Access-Control-Expose-Headers', 'Content-Disposition'); 
         res.setHeader('Content-Disposition', `attachment; filename="${title}.mp3"`);
         res.setHeader('Content-Type', 'audio/mpeg');
 
         ffmpeg(stream).audioBitrate(128).toFormat('mp3').pipe(res, { end: true });
 
     } catch (error) {
-        res.status(500).send('Error downloading the audio');
+        res.status(500).send(`Error downloading the audio, ${error}`);
     }
 })
 
